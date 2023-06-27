5fde32449c64eea0d07eb3131547ea4e0563ee63
Aditya Dahal <toadityadahal@gmail.com>
2023-06-27 12:23:39 +0545

    DTO for Customer

---
+++ src/main/java/com/delivery/drone/Dto/CustomerDto.java
@@ -0,0 +1,13 @@
+package com.delivery.drone.Dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerDto {
+    private int id;
+    private  String customerName;
+    private  String customerAddress;
+}
